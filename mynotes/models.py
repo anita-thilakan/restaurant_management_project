a1aab3eb7263563e2879dc1a1b038106f01c079a
anita-thilakan <anitathilakan@gmail.com>
2025-09-18 12:10:20 +0530

    added mynotes app with Note Model

---
+++ mynotes/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Note (models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    owner  = models.ForeignKey(User,on_delete = models.CASCADE)

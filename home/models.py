6c60875f5c6b4a500c3dfa1452939786284cb21f
anita-thilakan <anitathilakan@gmail.com>
2025-10-08 16:50:54 +0530

    changes under home and order models

--- home/models.py
+++ home/models.py
@@ -2,11 +2,7 @@ from django.db import models
 
 # Create your models here.
 class MenuCategory(models.Model):
-<<<<<<< HEAD
     name = models.CharField(max_length= 250, unique=True)
-=======
-    name = models.CharField(max_length = 250, unique = True)
->>>>>>> bac5e779490cd9da5091f129f83c3043fd071cc9
 
     def __str__(self):
         return self.name
--- orders/models.py
+++ orders/models.py
@@ -1,21 +1,15 @@
 from django.db import models
 
 # Create your models here.
-<<<<<<< HEAD
 
 #OrderStatus
 
 class OrderStatus(models.Model):
     name = models.CharField(max_length=100,unique= True)
-=======
-class OrderStatus(models.Model):
-    name = models.CharField(max_length = 100, unique= True)
->>>>>>> bac5e779490cd9da5091f129f83c3043fd071cc9
 
     def __str__(self):
         return self.name
 
-<<<<<<< HEAD
 class Coupons(models.Model):
     code = models.CharField(max_length= 10,unique=True)
     discount = models.DecimalField(max_digits=4,decimal_places=2)
@@ -26,7 +20,3 @@ class Order(models.Model):
     coupon = models.ForeignKey(Coupons,on_delete=models.SET_NULL,null=True)
 
     
-=======
-class Order(models.Model):
-    status = models.ForeignKey(OrderStatus, on_delete = models.SET_NULL, null=True)
->>>>>>> bac5e779490cd9da5091f129f83c3043fd071cc9
